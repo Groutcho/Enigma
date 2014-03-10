6938bedfb44f94f2dea8d5e0d577ed1f4cff9482
Sebastien Guimmara <sebastien.guimmara@gmail.com>
2014-03-10 20:58:54 +0100

    Added EnigmaDescriptor

---
+++ Enigma/Elements/EnigmaDescriptor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enigma.Data
+{
+    /// <summary>
+    /// Metadata of the enigma. Historical description, dates...
+    /// </summary>
+    public class EnigmaDescriptor
+    {
+        public string Name { get; }
+        public string Description { get; }
+        public string DateOfIntroduction { get; }
+    }
+}
