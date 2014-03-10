2931a2a5f1783c70a5379241f02ea6d2dd17789d
Sebastien Guimmara <sebastien.guimmara@gmail.com>
2014-03-10 20:57:06 +0100

    Added RotorDescriptor class

---
+++ Enigma/Elements/RotorDescriptor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enigma.Elements
+{
+    public class RotorDescriptor
+    {
+        public String Name { get; set; }
+        public String Type { get; set; }
+        public String Id { get; set; }
+        public String Mapping { get; set; }
+        public String Date { get; set; }
+        public String Model { get; set; }
+    }
+}
